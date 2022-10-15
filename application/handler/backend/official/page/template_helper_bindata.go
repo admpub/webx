0e0b566a694ae5d73766fa8d032a9b3ec38acc58
Hank Shen <swh@admpub.com>
2022-10-15 11:52:00 +0800

    update

--- application/handler/backend/official/page/template_helper_bindata.go
+++ application/handler/backend/official/page/template_helper_bindata.go
@@ -3,14 +3,5 @@
 
 package page
 
-import (
-	"net/http"
-
-	xbindata "github.com/admpub/webx/application/library/bindata"
-)
-
 func initTemplateBindata() {
-	for _, tmplDir := range xbindata.FrontendTemplateDirs.TmplDirs() {
-		templateDiskFS.Register(http.Dir(tmplDir))
-	}
 }
--- application/handler/backend/official/page/template_helper_none.go
+++ application/handler/backend/official/page/template_helper_none.go
@@ -3,5 +3,14 @@
 
 package page
 
+import (
+	"net/http"
+
+	xbindata "github.com/admpub/webx/application/library/bindata"
+)
+
 func initTemplateBindata() {
+	for _, tmplDir := range xbindata.FrontendTemplateDirs.TmplDirs() {
+		templateDiskFS.Register(http.Dir(tmplDir))
+	}
 }
