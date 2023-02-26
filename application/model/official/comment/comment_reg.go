7164c3ee67d5c8ef89302edd032334847fb0e768
Hank Shen <swh@admpub.com>
2023-02-26 13:20:49 +0800

    update

--- application/model/official/comment/comment_reg.go
+++ application/model/official/comment/comment_reg.go
@@ -4,7 +4,6 @@ import (
 	"github.com/webx-top/db"
 	"github.com/webx-top/echo"
 
-	"github.com/admpub/webx/application/dbschema"
 	"github.com/admpub/webx/application/model/official"
 	modelArticle "github.com/admpub/webx/application/model/official/article"
 )
@@ -24,24 +23,19 @@ func init() {
 			return m.UpdateField(nil, field, db.Raw(field+`+1`), db.Cond{`id`: id})
 		}, nil, nil
 	}))
-	modelArticle.ContentHideDetectorRegister(`comment`, `评论后才能查看`, func(
-		customer *dbschema.OfficialCustomer,
-		article *dbschema.OfficialCommonArticle,
-		hideContent string,
-		args ...string,
-	) bool {
-		if customer == nil {
+	modelArticle.ContentHideDetectorRegister(`comment`, `评论后才能查看`, func(params *modelArticle.ContentHideParams) bool {
+		if params.Customer == nil {
 			return true
 		}
-		if customer.Uid > 0 { // 管理员不用回复
+		if params.Customer.Uid > 0 { // 管理员不用回复
 			return false
 		}
-		cmtM := NewComment(article.Context())
+		cmtM := NewComment(params.Context)
 		exists, _ := cmtM.Exists(nil, db.And(
 			db.Cond{`target_type`: modelArticle.GroupName},
-			db.Cond{`target_id`: article.Id},
+			db.Cond{`target_id`: params.Article.Id},
 			db.Cond{`owner_type`: `customer`},
-			db.Cond{`owner_id`: customer.Id},
+			db.Cond{`owner_id`: params.Customer.Id},
 		))
 		return !exists
 	}, `此处内容需要评论后方可阅读`)
