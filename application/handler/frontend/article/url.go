c98be5fb5b6ac06849c9d1162aa02baddbdd97e1
Hank Shen <swh@admpub.com>
2024-04-16 15:33:26 +0800

    update

--- application/handler/frontend/article/url.go
+++ application/handler/frontend/article/url.go
@@ -5,8 +5,10 @@ import (
 	"strings"
 	"time"
 
+	"github.com/admpub/nging/v5/application/library/common"
 	"github.com/admpub/nging/v5/application/library/config"
 	"github.com/admpub/webx/application/library/top"
+	"github.com/admpub/webx/application/middleware/sessdata"
 	"github.com/webx-top/com"
 	"github.com/webx-top/echo"
 	"github.com/webx-top/echo/code"
@@ -16,6 +18,11 @@ func Redirect(ctx echo.Context) error {
 	if ctx.IsPost() {
 		return verifyRedirectURL(ctx)
 	}
+	customer := sessdata.Customer(ctx)
+	if customer == nil {
+		return common.ErrUserNotLoggedIn
+	}
+	username := customer.Name
 	url := ctx.Query(`url`)
 	if len(url) == 0 {
 		return ctx.NewError(code.InvalidParameter, `网址无效`).SetZone(`url`)
@@ -36,11 +43,12 @@ func Redirect(ctx echo.Context) error {
 		return ctx.NewError(code.DataHasExpired, `网址已经失效`)
 	}
 	parts := strings.SplitN(url, `#`, 2)
-	parts[0] = com.WithURLParams(parts[0], `_token`, config.FromFile().Encode256(url+`||`+time.Now().Format(time.DateTime)))
+	parts[0] = com.WithURLParams(parts[0], `_token`, config.FromFile().Encode256(url+`||`+time.Now().Format(time.DateTime)+`||`+username), `customer`, username)
 	url = strings.Join(parts, `#`)
 	return ctx.Redirect(url, http.StatusFound)
 }
 
+// url, token, customer
 func verifyRedirectURL(ctx echo.Context) error {
 	url := ctx.Form(`url`)
 	data := ctx.Data()
@@ -48,6 +56,7 @@ func verifyRedirectURL(ctx echo.Context) error {
 		data.SetError(ctx.NewError(code.InvalidParameter, `网址无效`).SetZone(`url`))
 		return ctx.JSON(data)
 	}
+	username := ctx.Form(`customer`)
 	token := ctx.Form(`token`)
 	if len(token) == 0 {
 		data.SetError(ctx.NewError(code.InvalidParameter, `令牌无效`).SetZone(`token`))
@@ -55,16 +64,20 @@ func verifyRedirectURL(ctx echo.Context) error {
 	}
 	content := config.FromFile().Decode256(token)
 	if len(content) == 0 {
-		data.SetError(ctx.NewError(code.DataFormatIncorrect, `令牌分析失败`))
+		data.SetError(ctx.NewError(code.DataFormatIncorrect, `令牌分析失败`).SetZone(`token`))
 		return ctx.JSON(data)
 	}
-	parts := strings.SplitN(content, `||`, 2)
-	if len(parts) != 2 {
-		data.SetError(ctx.NewError(code.DataFormatIncorrect, `令牌分析后的格式不正确`))
+	parts := strings.SplitN(content, `||`, 3)
+	if len(parts) != 3 {
+		data.SetError(ctx.NewError(code.DataFormatIncorrect, `令牌分析后的格式不正确`).SetZone(`token`))
 		return ctx.JSON(data)
 	}
 	if parts[0] != url {
-		data.SetError(ctx.NewError(code.DataUnavailable, `令牌与网址不匹配`))
+		data.SetError(ctx.NewError(code.DataUnavailable, `令牌与网址不匹配`).SetZone(`url`))
+		return ctx.JSON(data)
+	}
+	if len(parts[2]) > 0 && parts[2] != username {
+		data.SetError(ctx.NewError(code.DataUnavailable, `令牌与客户不匹配`).SetZone(`customer`))
 		return ctx.JSON(data)
 	}
 	t, err := time.Parse(time.DateTime, parts[1])
@@ -73,7 +86,7 @@ func verifyRedirectURL(ctx echo.Context) error {
 		return ctx.JSON(data)
 	}
 	if t.Before(time.Now().Add(time.Minute * -5)) {
-		data.SetError(ctx.NewError(code.DataHasExpired, `令牌已经过期`))
+		data.SetError(ctx.NewError(code.DataHasExpired, `令牌已经过期`).SetZone(`token`))
 	} else {
 		data.SetInfo(`OK`, code.Success.Int())
 	}
