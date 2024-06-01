be977947651d1553ad032cc886e55233693c6dbb
Hank Shen <swh@admpub.com>
2024-06-01 15:40:37 +0800

    update

--- application/initialize/init.go
+++ application/initialize/init.go
@@ -1,6 +1,7 @@
 package initialize
 
 import (
+	_ "github.com/admpub/nging/v5/application/initialize/manager"
 	"github.com/admpub/nging/v5/application/registry/navigate"
 	_ "github.com/admpub/webx/application/initialize/backend"
 	_ "github.com/admpub/webx/application/initialize/frontend"
@@ -8,8 +9,9 @@ import (
 
 var nav = &navigate.List{}
 
-var Project = navigate.NewProject(`Webx`, `webx`, `/official/customer/index`, nav)
+var Project = navigate.NewProject(`内容管理`, `webx`, `/official/customer/index`, nav)
 
 func init() {
+	navigate.ProjectGet(`nging`).Name = `其它功能`
 	navigate.ProjectAdd(1, Project)
 }
