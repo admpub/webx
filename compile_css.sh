374971c16593a695a4d86a8b533546ec8b3888b4
Hank Shen <swh@admpub.com>
2024-09-10 19:47:18 +0800

    update

---
+++ compile_css.sh
@@ -0,0 +1,10 @@
+# brew install node-sass
+export rootDir="public/assets/frontend/"
+
+sass $rootDir/css/custom.css:$rootDir/css/custom.min.css\
+ $rootDir/blogdefault/css/style.css:$rootDir/blogdefault/css/style.min.css\
+ $rootDir/blogdefault/css/dark.css:$rootDir/blogdefault/css/dark.min.css\
+ --style=compressed --no-source-map --watch
+
+# $rootDir/css/frontend.scss:$rootDir/css/frontend.min.css\ #bug
+# ~/minifyCSS.sh frontend.css frontend.min.css
