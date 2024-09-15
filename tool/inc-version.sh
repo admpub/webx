b7281c76b3e165891f8e8959ef4b2560f1026317
Hank Shen <swh@admpub.com>
2024-09-15 11:32:48 +0800

    update

--- tool/inc-version.sh
+++ tool/inc-version.sh
@@ -2,7 +2,7 @@ export PKGPATH=github.com/admpub/webx
 export ENTRYDIR=${GOPATH}/src
 export WORKDIR=${PWD}
 
-export NGING_VERSION="2.1.8"
+export NGING_VERSION="1.0.0"
 export NGING_BUILD=`date +%Y%m%d%H%M%S`
 export NGING_COMMIT=`git rev-parse HEAD`
 export NGING_LABEL="stable"
