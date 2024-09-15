e4a8fdc92fd38c1f4d65b49e7e6ad0e74be12f4a
Hank Shen <swh@admpub.com>
2024-09-15 11:29:12 +0800

    update

--- tool/inc-build.sh
+++ tool/inc-build.sh
@@ -6,19 +6,19 @@ export DISTPATH="../dist"
 export OSVERSIONDIR=${NGING_EXECUTOR}_${GOOS}_${GOARCH}
 export RELEASEDIR=${DISTPATH}/${OSVERSIONDIR}
 mkdir ${RELEASEDIR}
-go build -tags "bindata official sqlite${BUILDTAGS}" -ldflags="-X main.BUILD_TIME=${NGING_BUILD} -X main.COMMIT=${NGING_COMMIT} -X main.VERSION=${NGING_VERSION} -X main.LABEL=${NGING_LABEL} -X main.BUILD_OS=${GOOS} -X main.BUILD_ARCH=${GOARCH} -s -w -extldflags '-static'" -o ${RELEASEDIR}/${NGING_EXECUTOR}${NGINGEX} ..
+go build -tags "bindata official${BUILDTAGS}" -ldflags="-X main.BUILD_TIME=${NGING_BUILD} -X main.COMMIT=${NGING_COMMIT} -X main.VERSION=${NGING_VERSION} -X main.LABEL=${NGING_LABEL} -X main.BUILD_OS=${GOOS} -X main.BUILD_ARCH=${GOARCH} -s -w -extldflags '-static'" -o ${RELEASEDIR}/${NGING_EXECUTOR}${NGINGEX} ..
 mkdir ${RELEASEDIR}/data
 mkdir ${RELEASEDIR}/data/logs
 cp -R ../data/ip2region ${RELEASEDIR}/data/ip2region
 
 
 mkdir ${RELEASEDIR}/config
-mkdir ${RELEASEDIR}/config/vhosts
+#mkdir ${RELEASEDIR}/config/vhosts
 
 #cp -R ../config/config.yaml ${RELEASEDIR}/config/config.yaml
 cp -R ../config/config.yaml.sample ${RELEASEDIR}/config/config.yaml.sample
 cp -R ../config/insert.* ${RELEASEDIR}/config/
-cp -R ../config/preupgrade.* ${RELEASEDIR}/config/
+# cp -R ../config/preupgrade.* ${RELEASEDIR}/config/
 cp -R ../config/ua.txt ${RELEASEDIR}/config/ua.txt
 
 export archiver_extension="tar.gz"
