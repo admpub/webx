59951e17f6dd4ebf7bd18e06b2d6750e3a86dee7
Hank Shen <swhbox@foxmail.com>
2024-09-25 13:36:39 +0800

    update

--- tool/start.sh
+++
@@ -1,2 +0,0 @@
-cd ~/webx_linux_amd64
-./webx -p 9998 --frontenddomain www.hifilm.top,backend.hifilm.top:9998
--- tool/upgrade.sh
+++ tool/upgrade.sh
@@ -1,7 +1,13 @@
 cd /home/webx
 ./webx service stop
 cd /home
-unzip webx_linux_amd64.zip -d ./webx
+
+if [ "$1" = "" ]; then
+  unzip ./webx_linux_amd64.zip -d ./webx
+else
+  tar -zxvf ./webx_linux_amd64.tar.gz -C ./webx
+fi
+
 cp -R ./webx/webx_linux_amd64/* ./webx
 rm -rf ./webx/webx_linux_amd64
 cd /home/webx
