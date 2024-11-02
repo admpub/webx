de648aa5ca9f08ca0a22318e69a66699818f485d
Hank Shen <swh@admpub.com>
2024-11-02 14:03:07 +0800

    update

--- application/handler/frontend/oauth/handler.go
+++ application/handler/frontend/oauth/handler.go
@@ -53,7 +53,7 @@ func successHandler(ctx echo.Context) error {
 	if err != nil || end {
 		return err
 	}
-	var next string
+	next := ctx.Form(echo.DefaultNextURLVarName)
 	_, err = checkOrUpdateUser(ctx, oauthM, ouser, func(ctx echo.Context) (bool, error) {
 		if !ctx.Queryx(`force-create`).Bool() { //绑定用户账号，除非强制指定了自动创建新账号
 			if !fromSession { // 如果gothUser数据不是来自session，则需要保存到session中以便于用户登录或注册账号后关联oauth数据
