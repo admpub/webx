8975c5eef90120601ff6ec22af540e2cf9d2caa7
Hank Shen <swh@admpub.com>
2024-12-03 19:04:36 +0800

    update

---
+++ public/assets/frontend/js/page/user/qrcode_scan.js
@@ -0,0 +1,46 @@
+$(function(){
+var resultContainer = document.getElementById('qr-reader-results'),lastDecodedText='',t=null;
+function clearTimeoutT(){
+  if(!t) return; 
+  clearTimeout(t);t=null;
+}
+function onScanSuccess(decodedText, decodedResult) {
+  if(lastDecodedText===decodedText) return;
+  lastDecodedText=decodedText;
+  clearTimeoutT();
+  t=setTimeout(function(){lastDecodedText=''},5000);
+  $.post(FRONTEND_URL+'/user/qrcode/scan',{data:decodedText},function(r){
+      if(r.Code!=1){
+        if(r.Code==-101){
+          resultContainer.innerText=decodedText
+          if(decodedText){
+            $('#btn-copy-qrcode.hidden').removeClass('hidden');
+            App.message({text:App.t('识别成功'),type:'success'});
+          }else{
+            $('#btn-copy-qrcode:not(.hidden)').addClass('hidden')
+            clearTimeoutT();
+          }
+          return;
+        }
+
+        resultContainer.innerText=r.Info
+        $('#btn-copy-qrcode:not(.hidden)').addClass('hidden')
+        lastDecodedText='';
+        clearTimeoutT();
+        return App.message({text:r.Info,type:'error'});
+      }
+      resultContainer.innerText=r.Info
+      $('#btn-copy-qrcode:not(.hidden)').addClass('hidden')
+      return App.message({text:r.Info,type:'success'});
+  },'json').error(function(){
+    lastDecodedText='';
+    clearTimeoutT();
+  });
+}
+function onScanFailure(error) { 
+  lastDecodedText='';
+}
+var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", {fps: 10, qrbox: 250, isShowingInfoIcon: false});
+html5QrcodeScanner.render(onScanSuccess,onScanFailure);
+attachCopy('#btn-copy-qrcode');
+})
