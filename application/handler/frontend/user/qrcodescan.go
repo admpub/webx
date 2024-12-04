84fad6df656432a756cf5643efb59851ac218e3b
Hank Shen <swh@admpub.com>
2024-12-04 14:20:40 +0800

    update

--- application/handler/frontend/user/qrcodescan.go
+++ application/handler/frontend/user/qrcodescan.go
@@ -36,76 +36,106 @@ func qrcodeScan(ctx echo.Context) error {
 		if len(encrypted) == 0 {
 			return ctx.NewError(code.InvalidParameter, `参数“%s”不能为空`, `data`).SetZone(`data`)
 		}
-		encrypted = com.URLSafeBase64(encrypted, false)
-		plaintext := config.FromFile().Decode256(encrypted)
-		if len(plaintext) == 0 {
-			return ctx.NewError(code.InvalidParameter, `解密失败`).SetZone(`data`)
+		if !strings.HasPrefix(encrypted, `coscms:`) {
+			return ctx.NewError(code.InvalidParameter, `非本系统可以识别的参数`).SetZone(`data`)
 		}
-		signInData := &QRSignIn{}
-		err := com.JSONDecodeString(plaintext, &signInData)
-		if err != nil {
+		encrypted = strings.TrimPrefix(encrypted, `coscms:`)
+		parts := strings.SplitN(encrypted, `:`, 2)
+		if len(parts) != 2 {
+			return ctx.NewError(code.InvalidParameter, `无效的参数`).SetZone(`data`)
+		}
+		action := parts[0]
+		decoder, ok := qrcodeDecoders[action]
+		if !ok {
+			return ctx.NewError(code.Unsupported, `不支持的数据操作“%s”`, action).SetZone(`data`)
+		}
+		encrypted = parts[1]
+		if err := decoder(ctx, encrypted); err != nil {
 			return err
 		}
-		//echo.Dump(echo.H{`qrcodeScan`: signInData})
-		if signInData.Expires < time.Now().Unix() {
-			return ctx.NewError(code.DataHasExpired, `二维码已经过期`).SetZone(`data`)
-		}
-		sessStore := ss.Get(ctx.SessionOptions().Engine)
-		if sessStore == nil {
-			return ctx.NewError(code.Unsupported, `不支持 session 存储引擎: %s`, ctx.SessionOptions().Engine)
-		}
-
-		// sign-in
-		customer := sessdata.Customer(ctx)
-		customerM := modelCustomer.NewCustomer(ctx).DisabledSession(true)
-		err = customerM.Get(nil, `id`, customer.Id)
-		if err != nil {
-			if err == db.ErrNoMoreRows {
-				return ctx.NewError(code.UserNotFound, `用户不存在`)
-			}
-			return err
-		}
-		co := modelCustomer.NewCustomerOptions(nil)
-		co.Name = customer.Name
-		co.SignInType = `qrcode`
-		co.ApplyOptions(
-			modelCustomer.CustomerPlatform(signInData.Platform),
-			modelCustomer.CustomerScense(signInData.Scense),
-			modelCustomer.CustomerDeviceNo(signInData.DeviceNo),
-			modelCustomer.CustomerMaxAgeSeconds(signInData.SessionMaxAge),
-			modelCustomer.CustomerSessionID(signInData.SessionID),
-			modelCustomer.CustomerIPAddress(signInData.IPAddress),
-		)
-		session.RememberMaxAge(ctx, signInData.SessionMaxAge)
-
-		err = customerM.FireSignInSuccess(co, co.SignInType)
-		if err != nil {
-			return err
-		}
-		customerCopy := customerM.ClearPasswordData()
-
-		tmpCookieName := ctx.SessionOptions().Name + `TMP`
-		session := sessions.NewSession(sessStore, tmpCookieName)
-		session.IsNew = false
-		session.ID = signInData.SessionID
-
-		// set session values
-		ipInfo, _ := ip2region.IPInfo(signInData.IPAddress)
-		ip2region.ClearZero(&ipInfo)
-		sEnv := &sessionguard.Environment{
-			UserAgent: signInData.UserAgent,
-			Location:  ipInfo,
-		}
-		session.Values[`customer_env`] = sEnv
-		session.Values[`customer`] = &customerCopy
-		session.Values[`deviceInfo`] = &co.DeviceInfo
-
-		err = sessStore.Save(ctx, session)
-		if err != nil {
-			return err
-		}
-		ctx.SetCookie(tmpCookieName, ``, -1)
-		return ctx.JSON(ctx.Data().SetInfo(ctx.T(`扫码登录操作成功`)))
+		return ctx.JSON(ctx.Data())
 	}
 	return ctx.Render(`user/qrcode/scan`, nil)
+}
+
+var qrcodeDecoders = map[string]func(ctx echo.Context, encrypted string) error{
+	`signin`: qrcodeSignIn,
+}
+
+func RegisterQRCodeDecoder(name string, decoder func(ctx echo.Context, encrypted string) error) {
+	qrcodeDecoders[name] = decoder
+}
+
+func qrcodeSignIn(ctx echo.Context, encrypted string) error {
+	encrypted = com.URLSafeBase64(encrypted, false)
+	plaintext := config.FromFile().Decode256(encrypted)
+	if len(plaintext) == 0 {
+		return ctx.NewError(code.InvalidParameter, `解密失败`).SetZone(`data`)
+	}
+	signInData := &QRSignIn{}
+	err := com.JSONDecodeString(plaintext, &signInData)
+	if err != nil {
+		return err
+	}
+	//echo.Dump(echo.H{`qrcodeScan`: signInData})
+	if signInData.Expires < time.Now().Unix() {
+		return ctx.NewError(code.DataHasExpired, `二维码已经过期`).SetZone(`data`)
+	}
+	sessStore := ss.Get(ctx.SessionOptions().Engine)
+	if sessStore == nil {
+		return ctx.NewError(code.Unsupported, `不支持 session 存储引擎: %s`, ctx.SessionOptions().Engine)
+	}
+
+	// sign-in
+	customer := sessdata.Customer(ctx)
+	customerM := modelCustomer.NewCustomer(ctx).DisabledSession(true)
+	err = customerM.Get(nil, `id`, customer.Id)
+	if err != nil {
+		if err == db.ErrNoMoreRows {
+			return ctx.NewError(code.UserNotFound, `用户不存在`)
+		}
+		return err
+	}
+	co := modelCustomer.NewCustomerOptions(nil)
+	co.Name = customer.Name
+	co.SignInType = `qrcode`
+	co.ApplyOptions(
+		modelCustomer.CustomerPlatform(signInData.Platform),
+		modelCustomer.CustomerScense(signInData.Scense),
+		modelCustomer.CustomerDeviceNo(signInData.DeviceNo),
+		modelCustomer.CustomerMaxAgeSeconds(signInData.SessionMaxAge),
+		modelCustomer.CustomerSessionID(signInData.SessionID),
+		modelCustomer.CustomerIPAddress(signInData.IPAddress),
+	)
+	session.RememberMaxAge(ctx, signInData.SessionMaxAge)
+
+	err = customerM.FireSignInSuccess(co, co.SignInType)
+	if err != nil {
+		return err
+	}
+	customerCopy := customerM.ClearPasswordData()
+
+	tmpCookieName := ctx.SessionOptions().Name + `TMP`
+	session := sessions.NewSession(sessStore, tmpCookieName)
+	session.IsNew = false
+	session.ID = signInData.SessionID
+
+	// set session values
+	ipInfo, _ := ip2region.IPInfo(signInData.IPAddress)
+	ip2region.ClearZero(&ipInfo)
+	sEnv := &sessionguard.Environment{
+		UserAgent: signInData.UserAgent,
+		Location:  ipInfo,
+	}
+	session.Values[`customer_env`] = sEnv
+	session.Values[`customer`] = &customerCopy
+	session.Values[`deviceInfo`] = &co.DeviceInfo
+
+	err = sessStore.Save(ctx, session)
+	if err != nil {
+		return err
+	}
+	ctx.SetCookie(tmpCookieName, ``, -1)
+	ctx.Data().SetInfo(ctx.T(`扫码登录操作成功`))
+	return err
 }
--- public/assets/frontend/js/page/user/qrcode_scan.js
+++ public/assets/frontend/js/page/user/qrcode_scan.js
@@ -4,20 +4,19 @@ function clearTimeoutT(){
   if(!t) return; 
   clearTimeout(t);t=null;
 }
-function onScanSuccess(decodedText, decodedResult) {
+function onScanSuccess(decodedText) {
   if(!decodedText||lastDecodedText===decodedText) return;
   lastDecodedText=decodedText;
   clearTimeoutT();
   t=setTimeout(function(){lastDecodedText=''},5000);
+  if(!String(decodedText).startsWith('coscms:')){
+    resultContainer.innerText=decodedText
+    $('#btn-copy-qrcode.hidden').removeClass('hidden');
+    App.message({text:App.t('识别成功'),type:'success'});
+    return;
+  }
   $.post(FRONTEND_URL+'/user/qrcode/scan',{data:decodedText},function(r){
       if(r.Code!=1){
-        if(r.Code==-101){
-          resultContainer.innerText=decodedText
-          $('#btn-copy-qrcode.hidden').removeClass('hidden');
-          App.message({text:App.t('识别成功'),type:'success'});
-          return;
-        }
-
         resultContainer.innerText=r.Info
         $('#btn-copy-qrcode:not(.hidden)').addClass('hidden')
         lastDecodedText='';
@@ -35,7 +34,7 @@ function onScanSuccess(decodedText, decodedResult) {
 function onScanFailure(error) { 
   //lastDecodedText='';
 }
-var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", {fps: 10, qrbox: 250, isShowingInfoIcon: false});
+var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", {fps: 10, qrbox: 250, rememberLastUsedCamera: true});
 html5QrcodeScanner.render(onScanSuccess,onScanFailure);
 attachCopy('#btn-copy-qrcode');
 })
