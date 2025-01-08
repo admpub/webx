60d9f66ab8e60ff46d73f447dafb3c45a48a17bb
Hank Shen <swh@admpub.com>
2025-01-08 19:55:58 +0800

    update

---
+++ tool/gen_translate.sh
@@ -0,0 +1,2 @@
+#go install github.com/admpub/i18n/cmd/fetchtext@latest
+fetchtext --src=../ --dist=../config/i18n/messages --default=zh-cn --translate=true --onlyExport=false --clean=true --translator=tencent --translatorConfig="appid=&secret="
