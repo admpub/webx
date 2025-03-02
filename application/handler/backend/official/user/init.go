ba0831d8deec7db7f6cf3563c11ff3c176455b98
Hank Shen <swh@admpub.com>
2025-03-02 18:21:04 +0800

    update

--- application/handler/backend/official/user/init.go
+++ application/handler/backend/official/user/init.go
@@ -8,13 +8,13 @@ import (
 
 func init() {
 	httpserver.Backend.Router.RegisterToGroup(`/user`, func(g echo.RouteRegister) {
-		g.Route(`GET,POST`, `/message/unread_count`, MessageUnreadCount)
-		g.Route(`GET,POST`, `/message/inbox`, MessageInbox)
-		g.Route(`GET,POST`, `/message/outbox`, MessageOutbox)
-		g.Route(`GET,POST`, `/message/system`, SystemMessage)
-		g.Route(`GET,POST`, `/message/delete`, MessageDelete)
-		g.Route(`GET,POST`, `/message/view/:type/:id`, MessageView)
-		g.Route(`GET,POST`, `/message/send`, MessageSendHandler)
+		g.Route(`GET,POST`, `/message/unread_count`, MessageUnreadCount).SetName(`admin.message.unreadCount`)
+		g.Route(`GET,POST`, `/message/inbox`, MessageInbox).SetName(`admin.message.inbox`)
+		g.Route(`GET,POST`, `/message/outbox`, MessageOutbox).SetName(`admin.message.outbox`)
+		g.Route(`GET,POST`, `/message/system`, SystemMessage).SetName(`admin.message.system`)
+		g.Route(`GET,POST`, `/message/delete`, MessageDelete).SetName(`admin.message.delete`)
+		g.Route(`GET,POST`, `/message/view/:type/:id`, MessageView).SetName(`admin.message.view`)
+		g.Route(`GET,POST`, `/message/send`, MessageSendHandler).SetName(`admin.message.send`)
 	})
 
 	httpserver.Backend.Dashboard.TopButtons.Register(&dashboard.Button{
