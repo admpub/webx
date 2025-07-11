a2ca8c02898015898ac8c6779aaedcbdc6ed291e
Hank Shen <swh@admpub.com>
2025-07-11 20:41:52 +0800

    update

--- public/assets/backend/js/page/message/message.js
+++ public/assets/backend/js/page/message/message.js
@@ -1,13 +1,19 @@
 $(function(){
     $.get(BACKEND_URL+'/user/message/unread_count',{},function(r){
       if(r.Code!=1) return App.message({title:App.i18n.SYS_INFO,text:r.Info,type:'error'});
+      var hasNew = false;
       if(r.Data.user>0){
         $('#user-msg-count-label').removeClass('hidden').text(r.Data.user);
         $('#msg-inbox-count-label').removeClass('hidden').text(r.Data.user);
+        hasNew = true;
       }
       if(r.Data.system>0){
         $('#system-msg-count-label').removeClass('hidden').text(r.Data.system);
         $('#msg-sys-count-label').removeClass('hidden').text(r.Data.system);
+        hasNew = true;
+      }
+      if(hasNew){
+        $('#square-buttons-hasnew').removeClass('hidden');
       }
     },'json');
 });
