2a4215ce00ffdbe4605f8c3340158f0efcde06ef
Hank Shen <swh@admpub.com>
2025-08-19 13:45:09 +0800

    update

--- config/insert.navigate.sql
+++ config/insert.navigate.sql
@@ -1,3 +1,3 @@
 INSERT INTO `official_common_navigate` (`id`, `type`, `link_type`, `parent_id`, `has_child`, `level`, `title`, `url`, `ident`, `remark`, `sort`, `disabled`, `target`, `created`, `updated`) VALUES
-(1,	'default',	'custom',	0,	'N',	0,	'首页',	'/',	'',	'',	5000,	'N',	'',	1581589921,	1581599584),
-(2,	'default',	'category',	0,	'N',	0,	'分类',	'/articles',	'',	'',	5000,	'N',	'',	1581594682,	1581595885);
+(1,	'default',	'custom',	0,	'N',	0,	'首页',	'/',	'',	'',	0,	'N',	'',	1581589921,	1581599584),
+(2,	'default',	'article-category',	0,	'N',	0,	'全部文章',	'/articles',	'',	'',	5000,	'N',	'',	1581594682,	1581595885);
