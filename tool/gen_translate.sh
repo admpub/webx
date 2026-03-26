6cfa4eb6760549b35fd00ad656a8267481281b9f
Hank Shen <swhbox@foxmail.com>
2026-03-26 11:08:51 +0800

    update

--- tool/gen_translate.sh
+++ tool/gen_translate.sh
@@ -2,4 +2,4 @@
 fetchtext --src=../ --dist=../config/i18n/messages --default=zh-cn --translate=true --onlyExport=false --clean=true\
  --translator=tencent --translatorConfig="appid=&secret="\
  --vendorDirs="github.com/admpub/nging"\
- --envFile="../../nging/tool/translator_tencent.env" --onlyTranslateIncr=true
+ --envFile="$HOME/go/src/github.com/admpub/nging/tool/.translator_tencent.env" --onlyTranslateIncr=true
