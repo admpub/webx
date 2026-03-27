d55d7b026daca0eb3f400aa209f1e0d3b85b655c
Hank Shen <swhbox@foxmail.com>
2026-03-27 13:35:15 +0800

    update

--- application/handler/frontend/article/init.go
+++ application/handler/frontend/article/init.go
@@ -4,15 +4,17 @@ import (
 	"github.com/webx-top/echo"
 
 	_ "github.com/admpub/webx/application/handler/frontend/article/user"
+	"github.com/coscms/webcore/library/httpserver"
 	"github.com/coscms/webfront/initialize/frontend"
 )
 
 func init() {
 	frontend.Register(func(g echo.RouteRegister) {
-		g.Route(`GET`, `/article/<id:\d+>`, Detail).SetName(`article.detail`)
-		g.Route(`GET`, `/article/<id:\d+>/:op`, Detail).SetName(`article.detailWithOp`)
-		g.Route(`GET`, `/articlesBy/:sourceTable/:sourceId`, ArticleListBy).SetName(`article.listBy`)
-		g.Route(`GET`, `/articles`, List).SetName(`article.list`)
+		hmw := httpserver.Frontend.GetHandlerMiddlewares
+		g.Route(`GET`, `/article/<id:\d+>`, Detail, hmw(`article.detail`)...).SetName(`article.detail`)
+		g.Route(`GET`, `/article/<id:\d+>/:op`, Detail, hmw(`article.detailWithOp`)...).SetName(`article.detailWithOp`)
+		g.Route(`GET`, `/articlesBy/:sourceTable/:sourceId`, ArticleListBy, hmw(`article.listBy`)...).SetName(`article.listBy`)
+		g.Route(`GET`, `/articles`, List, hmw(`article.list`)...).SetName(`article.list`)
 		g.Route(`POST`, `/article/like`, ArticleLike).SetName(`article.like`)
 		g.Route(`POST`, `/article/hate`, ArticleHate).SetName(`article.hate`)
 		g.Route(`POST`, `/article/collect`, ArticleCollect).SetName(`article.collect`)
