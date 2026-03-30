8247dea17c3bec477222af8f867370044692493b
Hank Shen <swhbox@foxmail.com>
2026-03-30 13:16:40 +0800

    update

--- public/assets/frontend/js/page/comment.js
+++ public/assets/frontend/js/page/comment.js
@@ -2,6 +2,6 @@ $(function() {
     initPageAllCommentForm();
     var $container = $('#comment-list-container');
     if($container.length > 0 && $container.has('[scroll-load]')) {
-        $container.scrollLoading({callback:function() {commentList(1);},attr:'data-url'});
+        $container.scrollLoading({callback:function(){commentList(1);},attr:''});//attr:设置为空避免自动加载，默认为data-url
     }
 });
